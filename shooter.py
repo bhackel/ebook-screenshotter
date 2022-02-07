d783c63838f4b6d95d02ccbc8b327e4ac8372c69
Bryce Hackel <34104885+bhackel@users.noreply.github.com>
2022-02-07 01:31:50 -0800

    update something

--- shooter.py
+++ shooter.py
@@ -8,11 +8,21 @@ class Shooter:
 
     def __init__(self):
         self.screenshot_path = "./screenshots/"
-        self.num_pages = 850
-        self.X1 = 850
-        self.Y1 = 137
-        self.X2 = 2127
-        self.Y2 = 1771
+        self.num_pages = 4
+        self.X1 = 1852
+        self.Y1 = 269
+        self.X2 = 2558
+        self.Y2 = 1320
+
+        # 1616, 269, 2793, 1791
+        # 1444, 269, 2966, 1446
+
+        # Comment below and use single mode to include the boundaries in the image
+        # to make sure the pixel locations are correct. Check in Photoshop/image editor
+        self.X1 += 1
+        self.Y1 += 1
+        self.X2 -= 1
+        self.Y2 -= 1
 
     def start_grab(self):
         time.sleep(5)
@@ -21,11 +31,18 @@ class Shooter:
             im = ImageGrab.grab(bbox=(self.X1,self.Y1,self.X2,self.Y2))
             if old_im == im:
                 sys.exit()
-            filename = "{}image{:03d}.png".format(self.screenshot_path, i)
+            filename = "{}image{:03d}.png".format(self.screenshot_path, i+136)
             im.save(filename)
-            keyboard.send('ctrl+page Down')
+            print(f"captured {i} image, advancing...")
+
+            # Change this keybind to the shortcut for "next page" in the book
+            keyboard.send('right')
+
+            # Increase sleep delay if the book takes a bit to load
             time.sleep(2)
+           
             old_im = im
+        print("Finished, run shots_to_pdf.py to convert to a pdf")
 
     def single(self):
         time.sleep(3)
@@ -33,7 +50,11 @@ class Shooter:
         im.save(self.screenshot_path + "imagetest.png")
 
 if __name__ == '__main__':
+    time.sleep(2)
     if not os.path.exists('screenshots'):
         os.makedirs('screenshots')
     s = Shooter()
+    
+    # Switch between the two modes below for testing and full capture
+    #s.single()
     s.start_grab()
--- shots_to_pdf.py
+++ shots_to_pdf.py
@@ -3,30 +3,55 @@ import os
 from PIL import Image
 
 class To_PDF:
-	def convert(self):
-		screenshot_path = "./screenshots/"
-		image_files = os.listdir(screenshot_path)
-		image_files.sort()
+    def convert(self):
+        screenshot_path = "./screenshots/"
+        image_files = os.listdir(screenshot_path)
+        image_files.sort()
 
-		with open("output.pdf", "wb") as f:
-		    f.write(img2pdf.convert([screenshot_path+i for i in image_files]))
-	
-	def convert_2(self):
-		###not working
-		screenshot_path = "./screenshots/"
-		image_files = os.listdir(screenshot_path)
-		image_files.sort()
+        with open("output.pdf", "wb") as f:
+            f.write(img2pdf.convert([screenshot_path+i for i in image_files]))
+    
+    def convert_2(self):
+        ###not working
+        screenshot_path = "./screenshots/"
+        image_files = os.listdir(screenshot_path)
+        image_files.sort()
 
-		with Image.open(screenshot_path+image_files[0]) as im1:
-			im1.save("output2.pdf")
+        with Image.open(screenshot_path+image_files[0]) as im1:
+            im1.save("output2.pdf")
 
-		for image_file in image_files[1:]:
-			image = Image.open(screenshot_path+image_file)
-			pdf = Image.open("output2.pdf")
-			pdf.save("output2.pdf",save_all=True, append_images=[image])
-			pdf.close()
-			image.close()
+        for image_file in image_files[1:]:
+            image = Image.open(screenshot_path+image_file)
+            pdf = Image.open("output2.pdf")
+            pdf.save("output2.pdf",save_all=True, append_images=[image])
+            pdf.close()
+            image.close()
+
+    def remove_transparency(self, bg_colour=(255, 255, 255)):
+        ## maybe working
+        screenshot_path = "./screenshots/"
+        image_files = os.listdir(screenshot_path)
+        image_files.sort()
+
+        for image_file in image_files:
+            im = Image.open(screenshot_path+image_file)
+            # Only process if image has transparency (http://stackoverflow.com/a/1963146)
+            if im.mode in ('RGBA', 'LA') or (im.mode == 'P' and 'transparency' in im.info):
+
+                # Need to convert to RGBA if LA format due to a bug in PIL (http://stackoverflow.com/a/1963146)
+                alpha = im.convert('RGBA').split()[-1]
+
+                # Create a new background image of our matt color.
+                # Must be RGBA because paste requires both images have the same format
+                # (http://stackoverflow.com/a/8720632  and  http://stackoverflow.com/a/9459208)
+                bg = Image.new("RGBA", im.size, bg_colour + (255,))
+                bg.paste(im, mask=alpha)
+                bg.save(screenshot_path+image_file)
+                print(f"converted {image_file}")
+
+            else:
+                print(f'failed to convert {image_file}')
 
 
 if __name__ == "__main__":
-	To_PDF().convert()
+    To_PDF().convert()
